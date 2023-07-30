0c99c1c10e3bfe99667b2f399e4e65a1a25a8853
Ulises3soto <ulisotovega@gmail.com>
2023-07-29 22:14:16 -0300

    Se agrego el detalle del producto (html y css)

--- app.js
+++ app.js
@@ -9,9 +9,15 @@ const publicPath= path.resolve(__dirname, './public')
 
 
 
-app.use(express.static(publicPath))
+app.use(express.static("public"))
 
 
 app.listen(3000, ()=>{
      console.log('Servidor corriendo');
 }) 
+app.get('/', (req, res) => {
+     res.sendFile(path.join(__dirname,"/views/home.html"))
+ });
+app.get("/DetalleProducto",function (req,res){
+     res.sendFile(path.join(__dirname,"/views/DetalleProducto.html"))
+ })
