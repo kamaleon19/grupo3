bfc56e35bcb24653b73834d9a7f81e6faff533f6
LuisAlberto23 <cesarluis155alberto@gmail.com>
2023-09-04 01:31:02 -0300

    subo MVC terminado

--- controllers/mainControllers.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-let mainController = {
-    home : (req, res) => {
-        res.sendFile(path.join(__dirname,"../views/home.html"))
-    }
-}
-
-module.exports= mainController
